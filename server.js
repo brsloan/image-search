54e14cd09d9f754ae78f2da985c53ae1a96c9e3d
Benjamin Sloan <brsloan@gmail.com>
2016-01-17 06:02:52 +0000

    Added explantory index.html, updated readme, package.json, etc.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ require('dotenv').load();
 
 mongoose.connect(process.env.MONGO_URI);
 
+app.use('/public', express.static(process.cwd() + '/public'));
+
 routes(app);
 
 var port = process.env.PORT || 8080;
